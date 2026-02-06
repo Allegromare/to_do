4758d2bdc6140b9aa241f839e5c54c198caf41c5
Allegromare <Allegromare@gmail.com>
2026-02-06 21:50:46 +0100

    (no commit message provided)

--- src/main.py
+++ src/main.py
@@ -2,32 +2,14 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-
-    def decrement_click(e):
-        counter.data -= 1
-        counter.value = str(counter.data)    
-
-
+    page.title = "Tasker"
     page.add(
         ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=ft.Row(
-                    controls=[
-                        ft.IconButton(icon=ft.Icons.REMOVE, on_click=decrement_click, icon_size=40),
-                        counter,
-                        ft.IconButton(icon=ft.Icons.ADD, on_click=increment_click, icon_size=40),
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    spacing=30,
-                ),
-                alignment=ft.Alignment.CENTER,
-            ),
+            ft.Column(
+                [
+                    ft.Text("Tasker App"),
+                ]
+            )
         )
     )
 
